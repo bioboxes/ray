7bc0a0817698b8ad7838de051889963f374e76d9
PlsDontPanic <pbelmann@cebitec.uni-bielefeld.de>
2015-03-18 23:02:01 +0100

    CONTIG and SCAFFOLD type added close #3

--- bbx/bin/command_handler.py
+++ bbx/bin/command_handler.py
@@ -53,6 +53,7 @@ if __name__ == "__main__":
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         yaml_output = out_dir + "/out.yaml"
-        output_data = {'version': '0.9.0', 'arguments': [{ "value": "ray/Contigs.fasta" , "type" : "fasta"}]}
+        output_data = {'version': '0.9.0', 'arguments': [{ "value": "ray/Contigs.fasta" , "type" : "CONTIG"},
+                                                         { "value": "ray/Scaffolds.fasta" , "type" : "SCAFFOLD"}]}
         stream = open(yaml_output, 'w')
         yaml.dump(output_data,default_flow_style=False,stream=stream)
