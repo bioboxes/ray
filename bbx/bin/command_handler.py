60ffd26fad6ad9a0b5e4b4681629839d1ed37141
PlsDontPanic <pbelmann@cebitec.uni-bielefeld.de>
2015-03-19 17:14:32 +0100

    relative input path in yaml required

--- bbx/bin/command_handler.py
+++ bbx/bin/command_handler.py
@@ -4,17 +4,21 @@ import argparse
 import yaml
 import os
 
+
 class Assembler:
     def __init__(self, **entries):
         self.__dict__.update(entries)
 
+
 if __name__ == "__main__":
-    #Parse arguments
+
+    bbx_input_dir = "/bbx/input/"
+    # Parse arguments
     parser = argparse.ArgumentParser(description='Parses input yaml')
     parser.add_argument('-i', '--input_yaml', dest='i', nargs=1,
                         help='YAML input file')
     parser.add_argument('-o', '--output_path', dest='o', nargs=1,
-                    help='Output path')
+                        help='Output path')
     args = parser.parse_args()
 
     #get input files
@@ -35,25 +39,26 @@ if __name__ == "__main__":
     for argument in assembler.arguments:
         if argument.has_key("fastq"):
             fastq = argument["fastq"]
-    if(len(fastq)==0):
+    if (len(fastq) == 0):
         raise ValueError("YAML is not in a valid format. Please check the definition on bioboxes.")
 
     #run ray
     output = output_path + "/ray"
     input_type = ""
-    if(fastq[0].get("type") == "paired"):
+    if (fastq[0].get("type") == "paired"):
         input_type = "-i"
-    elif(fastq[0].get("type") == "single"):
+    elif (fastq[0].get("type") == "single"):
         input_type = "-s"
 
-    command = "mpiexec -n 8 /opt/bin/Ray " + input_type + " " + fastq[0].get("path") + " -k 31 -o " + output
+    command = "mpiexec -n 8 /opt/bin/Ray " + input_type + " " + bbx_input_dir + fastq[0].get(
+        "path") + " -k 31 -o " + output
     exit = os.system(command)
-    if(exit==0):
+    if (exit == 0):
         out_dir = output_path + "/bbx"
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         yaml_output = out_dir + "/out.yaml"
-        output_data = {'version': '0.9.0', 'arguments': [{ "value": "ray/Contigs.fasta" , "type" : "CONTIG"},
-                                                         { "value": "ray/Scaffolds.fasta" , "type" : "SCAFFOLD"}]}
+        output_data = {'version': '0.9.0', 'arguments': [{"value": "ray/Contigs.fasta", "type": "contig"},
+                                                         {"value": "ray/Scaffolds.fasta", "type": "scaffold"}]}
         stream = open(yaml_output, 'w')
-        yaml.dump(output_data,default_flow_style=False,stream=stream)
+        yaml.dump(output_data, default_flow_style=False, stream=stream)
